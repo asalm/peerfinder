219a8df1af1dbdd5c887a1c2a9524435957a799c
Andre <asalmeri@stud.hs-bremen.de>
2019-07-09 14:53:52 +0200

    integrated API-Request-Object

--- src/components/GroupView.vue
+++ src/components/GroupView.vue
@@ -59,6 +59,9 @@ export default {
   },
   methods:{
 
+  },
+  created(){
+      console.log(this.$api.usr);
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -5,13 +5,7 @@ import App from './App.vue';
 import 'buefy/dist/buefy.css';
 //import Router_func from '../src/assets/route_func';
 
-var url = new URL(window.location.href);
 
-//Router Config Stuff
-let ParentMachine = 'https://www.impact-lab.tools/';
-let usr_tkn = url.searchParams.get("token")
-let app_tkn = '9a056d8d4072e4f865aa7a3e5ff343cfd3b4a9e6ad8b6e9f9b10e302ed15cbd9e1b330b6328dea0b8e184bf3c2bdadaaa86ed97eed0c383cc9b73bef9c98d7c6';
-var rr;
 
 Vue.config.productionTip = false;
 
@@ -21,33 +15,40 @@ initVue();
 
 
 async function initVue() {
+  await initScript().then(async function(r) {
+    //var api = r;
+
+    Vue.prototype.$api = r;
+    Vue.prototype.$token = "9a056d8d4072e4f865aa7a3e5ff343cfd3b4a9e6ad8b6e9f9b10e302ed15cbd9e1b330b6328dea0b8e184bf3c2bdadaaa86ed97eed0c383cc9b73bef9c98d7c6";
+
     new Vue({
       router,
       render: h => h(App)
-    }).$mount('#app');
-  //});
-
-
+    }).$mount('#app')
+  });
 }
-/*
+
+
 function initScript(){
 
-
   return new Promise(async function(res, rej){
-
+    var url = new URL(window.location.href);
 
     var config = {
-      "host" : ParentMachine,
-      "user_token" : usr_tkn,
-      "app_token" : app_tkn,
-      "schemaName" : "peerf"
+      "host":"http://impact-lab.tools",
+      "user_token": url.searchParams.get("token"),
+      "app_token": "9a056d8d4072e4f865aa7a3e5ff343cfd3b4a9e6ad8b6e9f9b10e302ed15cbd9e1b330b6328dea0b8e184bf3c2bdadaaa86ed97eed0c383cc9b73bef9c98d7c6",
+      "schemaName": "peer"
     };
-
     var _r = new Router_func(config);
+    try{
     await _r.setup();
+    }catch(e){
+      rej("Error");
+    }
+    
     res(_r);
-
+    
   });
+}
 
-
-}*/
